ca17037619f1287d799c8cadafd6a371a81d54e3
gnoejh <hj628853GI!>
2022-01-04 22:43:02 +0900

    update

--- Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
+++ Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import java.lang.Thread.sleep
 
@@ -11,24 +13,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //button2 callback
-        val button: Button = findViewById(R.id.button2)
-        button.setOnClickListener {
-            //long version
-            val toast = Toast.makeText(applicationContext,"Hello Toast!",Toast.LENGTH_LONG)
-            toast.show()
-
-            //sleep
-            sleep(5000)
-
-            //short version
-            Toast.makeText(applicationContext,"Hello Toast 2!",Toast.LENGTH_LONG).show()
-        }
     }
 
     //part of activity, diable the above button.setOnClickListner
     fun onClickButton(v:View){
-        Toast.makeText(applicationContext,"Hello Toast 3",Toast.LENGTH_LONG).show()
+
+        var edit_text:EditText = findViewById(R.id.editText)
+        var text_view:TextView = findViewById(R.id.textView)
+        if (edit_text.text.isNotEmpty()){
+            val dollarValue = edit_text.text.toString().toFloat()
+            val euroValue = dollarValue * 0.85f
+            text_view.text = euroValue.toString()
+        }
+        else {
+            text_view.text = "No value"
+        }
+
+
+
     }
 }
