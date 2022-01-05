f00bdd36759b433f01bb43741d3bb10c05a40732
gnoejh <hj628853GI!>
2022-01-05 22:26:56 +0900

    update

--- Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
+++ Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
@@ -4,26 +4,57 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
-import com.example.mystarter.databinding.ActivityMainBinding    //2 notice the name
+import com.example.mystarter.databinding.ActivityMainBinding
+import android.util.Log                                                 //1
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding           //3
+    private val TAG = "HJ"                                              //2
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)   //4
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-//        setContentView(R.layout.activity_main)
-        setContentView(binding.root)                            //5
-
-
+        setContentView(binding.root)
+        Log.i(TAG, "onCreate")
     }
 
     fun sayHello(v: View){
-//        var textview: TextView = findViewById(R.id.textView)
-//        textView.text = "Hello, world!"  //use view object directly
-        binding.textView.text = "Hello, world!"                 //6
+        binding.textView.text = "Hello, world!"
+    }
+
+    //other functions
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG, "onRestoreInstanceState")
     }
 }
