2220eaf02319e5ca3f42686a6066c2be52081d8d
gnoejh <hj628853GI!>
2022-01-05 21:51:13 +0900

    update

--- Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
+++ Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*  //for synthetic properties
+import com.example.mystarter.databinding.ActivityMainBinding    //2 notice the name
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding           //3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)   //4
+
+//        setContentView(R.layout.activity_main)
+        setContentView(binding.root)                            //5
+
+
     }
 
     fun sayHello(v: View){
 //        var textview: TextView = findViewById(R.id.textView)
-        textView.text = "Hello, world!"  //use view object directly
+//        textView.text = "Hello, world!"  //use view object directly
+        binding.textView.text = "Hello, world!"                 //6
     }
 }
