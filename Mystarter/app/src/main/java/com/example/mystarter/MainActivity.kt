9d80050b36093e1e80790f5bf1255f80cbf2ab49
gnoejh <hj628853GI!>
2022-01-05 12:25:26 +0900

    update

--- Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
+++ Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
@@ -9,3 +9,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//TODO Kotlin keywords
+
+//Kotlin Hard Keywords
+//Following is a list of hard keywords and they cannot be used as identifiers
+/*
+as	        as?	        break	        class
+continue	do	        else	        false
+for	        fun	        if	            in
+!in	        interface	is	            !is
+null	    object	    package	        return
+super	    this	    throw	        true
+try	        typealias	typeof	        val
+var	        when	    while
+*/
+//Kotlin Soft Keywords
+//Following is the list of keywords (soft) in the context when they are applicable and can be used as identifiers in other contexts
+/*
+by	        catch	    constructor	    delegate
+dynamic	    field	    file	        finally
+get	        import	    init	        param
+property	receiver	set	            setparam
+value	    where
+*/
