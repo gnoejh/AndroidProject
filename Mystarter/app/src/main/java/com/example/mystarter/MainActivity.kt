2c463909c7b5144ec1b874ff6a7786434859da0e
gnoejh <hj628853GI!>
2022-01-05 12:07:54 +0900

    update

--- Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
+++ Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
@@ -7,5 +7,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main()
+    }
+
+    fun main(){
+        //print vs println
+        print("Hello, "); print("world!")
+        println("")
+        println("Hello, world!!")
+
+        //print arguments
+        println(200)
+        println("200")
+        println(5 + 3*7 + 10/2)
+
+        //semicolon
+        println("I'm without semi-colon")
+        println("I'm with semicolon");
     }
 }
