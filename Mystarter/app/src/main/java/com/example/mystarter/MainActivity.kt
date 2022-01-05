4483084cceca68af64caebe02be691699d374834
gnoejh <hj628853GI!>
2022-01-05 21:14:26 +0900

    update

--- Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
+++ Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.mystarter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sayHello(){
+        var textview: TextView = findViewById(R.id.textView)
+        textview.text = "Hello, world!"
     }
 }
