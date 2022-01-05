b427a36a056f5f0c38ce5c29b9ef1acfdf8feedc
gnoejh <hj628853GI!>
2022-01-05 12:16:41 +0900

    update

--- Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
+++ Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
@@ -9,3 +9,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//TODO comments
+
+//This is a single-line comment
+fun main(){
+    println("Hello, world!") // This is also a single-line comment
+}
+
+/* This is a muli-line comment and it can span
+ * as many as lines as you like
+ */
+
+
+/* This is a nested comments consisting of
+ *multi-line- and single-line comments
+ /* This is a nested comment */
+ // Another nested comment
+ */
