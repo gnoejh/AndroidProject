ed82aede61351aa4eb55f90c87a44e043a410c0d
gnoejh <hj628853GI!>
2022-01-05 11:54:43 +0900

    update

--- Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
+++ Mystarter/app/src/main/java/com/example/mystarter/MainActivity.kt
@@ -7,5 +7,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        main()
+        main (arrayOf("a","b","c"))
+
+    }
+    //entry point
+    fun main(){
+        var string: String = "Hello, world!!!"
+        println("$string")
+    }
+
+    //entry point with parameters
+    fun main(args: Array<String>){
+        for (i in 0..2){
+            println(args[i])
+        }
     }
 }
